3b0457d06de68f75d321c955f6d335a789ad79f1
marijevdgeest <marijevdgeest@gmail.com>
2015-09-22 16:13:35 +0200

    fixed path for decon-eqtl plots

--- molgenis-lncrna-explorer/src/main/resources/js/deconvolution.js
+++ molgenis-lncrna-explorer/src/main/resources/js/deconvolution.js
@@ -239,7 +239,7 @@
 	;
 
 	function getUri(attrs, num, filters) {
-		var uri = 'http://localhost:8080/api/v2/DeconvolutionPlot?attrs=' + attrs + '&num=' + num;
+		var uri = 'https://molgenis04.target.rug.nl/api/v2/DeconvolutionPlot?attrs=' + attrs + '&num=' + num;
 		var query = [];
 		if (filters.disease) {
 			query.push('disease==' + toRsqlValue(filters.disease));
