45785c4d433978b8b3de9213957927daa9f94d2a
marijevdgeest <marijevdgeest@gmail.com>
2015-11-16 17:15:33 +0100

    Implemented SNP search, responsive genes and windowframe

--- molgenis-lncrna-explorer/src/main/resources/js/lncrna-explorer.js
+++ molgenis-lncrna-explorer/src/main/resources/js/lncrna-explorer.js
@@ -1,6 +1,7 @@
 (function($, molgenis) {
 
 	var div = React.DOM.div;
+	var span = React.DOM.span;
 
 	var LncRNAExplorerClass = React.createClass({
 		displayName : 'LncRNAExplorer',
@@ -8,49 +9,88 @@
 		getInitialState : function() {
 			return {
 				genes : [],
+				snp : null,
+				genesToPlot : [],
+				windowSize : 250000
 			};
 		},
-		_handleSearch : function(genes) {
-			console.log(genes);
+		_onGenesSelection : function(genes) {
 			this.setState({
-				genes : genes
+				genes : genes.value,
+				genesToPlot : []
 			});
 		},
+		_onSearch : function() {
+			this.setState({
+				genesToPlot : this.state.genes
+			});
+		},
+		_onSnpSelect : function(snp) {
+			console.log(snp);
+			this.setState({
+				snp : snp.value
+			});
+			if (snp.value != null) {
+				$.get(
+						'http://localhost:8080/api/v2/GenePos?attrs=~id,EnsemblGeneID,AssociatedGeneName&q=Chromosome=q=' + snp.value.Chromosome + ';GeneStart=le='
+								+ (parseInt(snp.value.POS, 10) + this.state.windowSize) + ';GeneEnd=ge=' + (parseInt(snp.value.POS, 10) - this.state.windowSize)).then(
+						this._onGenesFound);
+			}
+		},
+		_onGenesFound : function(data) {
+			this.setState({
+				genes : data.items,
+				genesToPlot : []
+			});
+
+		},
+		_zoomIn : function() {
+			this.setState({
+				windowSize : this.state.windowSize / 2
+			});
+		},
+		_zoomOut : function() {
+			this.setState({
+				windowSize : this.state.windowSize * 2
+			});
+
+		},
+
 		render : function() {
 
 			var genePlots = [];
 
-			if (this.state.genes.length >= 2) {
-				genePlots = [ React.DOM.div({
+			if (this.state.genesToPlot.length >= 2) {
+				genePlots = [ div({
 					className : "row"
 				}, GenePlot({
-					genes : this.state.genes,
+					genes : this.state.genesToPlot,
 					scriptName : 'generateExpression%28rpkm%29Heatmap',
 					title : 'Cell type expression profile',
 					inputType : 'geneName'
-				})), React.DOM.div({
+				})), div({
 					className : "row"
 				}, GenePlot({
-					genes : this.state.genes,
+					genes : this.state.genesToPlot,
 					scriptName : 'correlationCoexpression',
 					title : 'Coexpression',
 					inputType : 'geneName'
-				})), React.DOM.div({
+				})), div({
 					className : "row"
 				}, GenePlot({
-					genes : this.state.genes,
+					genes : this.state.genesToPlot,
 					scriptName : 'grTcellHeatmap',
 					title : 'Gluten Specific T-cells',
 					inputType : 'geneID'
 				}), GenePlot({
-					genes : this.state.genes,
+					genes : this.state.genesToPlot,
 					scriptName : 'timecourse_gs_tcells',
 					title : 'Time course gluten specific T-cells',
 					inputType : 'geneID'
-				})), React.DOM.div({
+				})), div({
 					className : "row"
 				}, GenePlot({
-					genes : this.state.genes,
+					genes : this.state.genesToPlot,
 					scriptName : 'stimuliHeatmap',
 					title : 'Stimulated PMBC expression',
 					inputType : 'geneID'
@@ -60,36 +100,47 @@
 			return div({}, div({
 				className : 'row'
 			}, div({
+				className : 'col-md-1 col-md-offset-4'
+			}, molgenis.ui.Button({
+				id : 'zoom-in-button',
+				type : 'button',
+				style : 'info',
+				size : 'medium',
+				text : '+',
+				name : 'zoom-in',
+				disabled : false,
+				onClick : this._zoomIn
+			})), div({
+				className : 'col-md-1'
+			}, span({}, '' + this.state.windowSize)), div({
+				className : 'col-md-1'
+			}, molgenis.ui.Button({
+				id : 'zoom-out-button',
+				type : 'button',
+				style : 'info',
+				size : 'medium',
+				text : '-',
+				name : 'zoom-out',
+				disabled : false,
+				onClick : this._zoomOut
+			}))), div({
+				className : 'row'
+			}, div({
 				className : 'col-md-4 col-md-offset-4'
-			}, GenesSelectBox({
-				onSearch : this._handleSearch
-			}))), genePlots);
-		}
-	});
-
-	var LncRNAExplorer = React.createFactory(LncRNAExplorerClass);
-
-	var GenesSelectBoxClass = React.createClass({
-		displayName : 'GenesSelectBox',
-		propTypes : {
-			onSearch : React.PropTypes.func
-		},
-		getInitialState : function() {
-			return {
-				genes : [],
-			};
-		},
-		_onGenesSelection : function(event) {
-			this.setState({
-				genes : event.value
-			});
-		},
-		_onButtonPress : function() {
-			this.props.onSearch(this.state.genes);
-		},
-		render : function() {
-			return React.DOM.div({}, molgenis.ui.EntitySelectBox({
-				entity : 'BioMartGenes',
+			}, React.DOM.div({}, molgenis.ui.EntitySelectBox({
+				entity : 'SnpsToPlot',
+				mode : 'view',
+				name : "name",
+				disabled : false,
+				readOnly : false,
+				multiple : false,
+				required : false,
+				placeholder : 'Please select a SNP',
+				focus : false,
+				value : [],
+				onValueChange : this._onSnpSelect
+			})), React.DOM.div({}, molgenis.ui.EntitySelectBox({
+				entity : 'GenePos',
 				mode : 'view',
 				name : "name",
 				disabled : false,
@@ -98,22 +149,22 @@
 				required : true,
 				placeholder : 'Please select one or more Genes',
 				focus : false,
-				value : [],
+				value : this.state.genes,
 				onValueChange : this._onGenesSelection
-			}), molgenis.ui.Button({
-				id : 'search-button',
+			})), molgenis.ui.Button({
+				id : 'plot-button',
 				type : 'button',
 				style : 'info',
 				size : 'medium',
-				text : 'Search',
-				name : 'Search',
+				text : 'Plot',
+				name : 'Plot',
 				disabled : false,
-				onClick : this._onButtonPress,
-			}));
+				onClick : this._onSearch,
+			}))), genePlots);
 		}
 	});
 
-	var GenesSelectBox = React.createFactory(GenesSelectBoxClass);
+	var LncRNAExplorer = React.createFactory(LncRNAExplorerClass);
 
 	var GenePlotClass = React.createClass({
 		displayName : 'GenePlot',
@@ -141,9 +192,10 @@
 		},
 		render : function() {
 			return React.DOM.div({
-				className : "col-md-6"
+				className : "col-md-6 col-sm-12"
 			}, React.DOM.h3({}, this.props.title), React.DOM.img({
-				src : 'http://localhost:8080/scripts/' + this.props.scriptName + '/run?genes=' + this._mapGenes()
+				src : 'http://localhost:8080/scripts/' + this.props.scriptName + '/run?genes=' + this._mapGenes(),
+				lowsrc : "css/select2-spinner.gif"
 			}));
 		}
 	});
